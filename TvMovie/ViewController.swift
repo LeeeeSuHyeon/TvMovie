09273baf306c399118f0472ef2e9dfc970e7416c
suhyeon lee <tngus0673@naver.com>
2024-07-29 22:09:39 +0900

    라이브러리 세팅(Kingfisher, SnapKit, RxSwift, RxAlamofire

---
+++ TvMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TvMovie
+//
+//  Created by 이수현 on 7/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
