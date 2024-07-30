d581c55cbcf0ac1045347d8b143bb2c060fd42b2
suhyeon lee <tngus0673@naver.com>
2024-07-30 16:42:01 +0900

    Generic 활용하여 네트워크 모듈 구현(RxSwift, RxAlamofire)

---
+++ TvMovie/Network/Network.swift
@@ -0,0 +1,31 @@
+//
+//  Network.swift
+//  TvMovie
+//
+//  Created by 이수현 on 7/30/24.
+//
+
+import Foundation
+import RxSwift
+import RxAlamofire
+
+
+class Network<T : Decodable> {
+    private let endPoint : String
+    private let queue : ConcurrentDispatchQueueScheduler
+    
+    init(endPoint: String) {
+        self.endPoint = endPoint
+        self.queue = ConcurrentDispatchQueueScheduler(qos: .background)
+    }
+    
+    func getItemList(path : String) -> Observable<T> {
+        let fullPath = "\(endPoint)\(path)?api_keys=\(APIKEY)&language=ko"
+        return RxAlamofire.data(.get, fullPath)
+            .observeOn(queue)
+            .debug()
+            .map { data -> T in
+                return try JSONDecoder().decode(T.self, from: data)
+            }
+    }
+}
