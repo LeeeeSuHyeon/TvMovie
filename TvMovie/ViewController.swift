aba9535799e85c53e429152fdb05fc07cd1acf5e
suhyeon lee <tngus0673@naver.com>
2024-08-11 15:06:19 +0900

    dobuleLayout 설정

--- TvMovie/ViewController.swift
+++ TvMovie/ViewController.swift
@@ -24,15 +24,38 @@ class ViewController: UIViewController {
     let disposeBag = DisposeBag()
     let buttonView = ButtonView()
     
-    var collectionView : UICollectionView = {
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+    lazy var collectionView : UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: self.createLayout())
         collectionView.register(NormalCollectionViewCell.self, forCellWithReuseIdentifier: NormalCollectionViewCell.id)
         
         return collectionView
     }()
+    
     let viewModel = ViewModel()
     let tvTrigger = PublishSubject<Void>()
     let movieTrigger = PublishSubject<Void>()
+    
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+        let config = UICollectionViewCompositionalLayoutConfiguration()
+        config.interSectionSpacing = 14
+        
+        return UICollectionViewCompositionalLayout(sectionProvider: {[weak self] sectionIndex, _ in
+            return self?.createDoubleSection()
+        }, configuration: config)
+    
+    }
+    
+    private func createDoubleSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 4, bottom: 8, trailing: 4)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(320))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        return section
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
